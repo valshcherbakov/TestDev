110cffe84f8d93a53e8631d62f2db4f4b1c85f16
lecik <lecik@DESKTOP-2VLREQB.fios-router.home>
2018-09-08 01:01:35 -0400

    another add

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World, Jopag!" );
         System.out.println("F...A");
-        System.out.println("WTF");
+        System.out.println("WTF11");
     }
 }
